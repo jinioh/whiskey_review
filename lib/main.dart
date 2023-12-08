73cc94b561344d89a39a817db436b529a9e6153e
jinioh <93959611+jinioh@users.noreply.github.com>
2023-12-08 14:45:45 +0900

    lib만 올림

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,17 @@
 import 'dart:ui';
 
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_application_1/firebase_options.dart';
 import 'package:flutter_application_1/provider/WhiskeyTastingNoteProvier.dart';
 import 'package:flutter_application_1/screens/review_write_screen.dart';
 import 'package:provider/provider.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(
     ChangeNotifierProvider(
       create: (context) => WhiskeyTastingNoteProvider(),
@@ -15,7 +21,8 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
